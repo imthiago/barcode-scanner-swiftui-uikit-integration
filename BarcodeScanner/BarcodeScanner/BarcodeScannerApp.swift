e43f681f8e5fb9525ee70873e38f7695c5aaf621
Thiago Oliveira <thiago@outlook.com.br>
2022-08-10 20:02:27 -0300

    Initial setup

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Thiago Oliveira on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}

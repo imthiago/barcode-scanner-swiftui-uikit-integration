d2b1a5c17b2ef8361626fcb72c946045217c83ef
Thiago Oliveira <thiago@outlook.com.br>
2022-08-10 21:44:05 -0300

    Moving coordinator

--- BarcodeScanner/BarcodeScanner/Screens/Views/UIKit Components/ScannerView.swift
+++ BarcodeScanner/BarcodeScanner/Screens/Views/UIKit Components/ScannerView.swift
@@ -22,27 +22,6 @@ struct ScannerView: UIViewControllerRepresentable {
     func makeCoordinator() -> Coordinator {
         Coordinator(scannerView: self)
     }
-
-    final class Coordinator: NSObject, ScannerViewControllerDelegate {
-        private let scannerView: ScannerView
-
-        init(scannerView: ScannerView) {
-            self.scannerView = scannerView
-        }
-
-        func didFind(barcode: String) {
-            scannerView.scannedCode = barcode
-        }
-
-        func didSurface(error: CameraError) {
-            switch error {
-            case .invalidDeviceInput:
-                scannerView.alertItem = AlertContext.invalidDeviceInput
-            case .invalidScannedValue:
-                scannerView.alertItem = AlertContext.invalidScannedType
-            }
-        }
-    }
 }
 
 struct ScannerView_Previews: PreviewProvider {
